67a66fd6a09d569b6325088447e20a0f7c497c6f
ramon8 <ramon.fer96@gmail.com>
2019-06-02 12:36:07 +0200

    nuevas animaciones

--- src/states/game.ts
+++ src/states/game.ts
@@ -8,7 +8,8 @@ export default class Game extends Phaser.State {
     private jumpTimer = 0;
 
     public preload(): void {
-        this.load.spritesheet('player_walk', require('assets/player_walk.png'), 16, 18, 22);
+        // this.load.spritesheet('player_walk', require('assets/player_walk.png'), 16, 18, 22);
+        this.load.spritesheet('player_anim', require('assets/pj_anim.png'), 16, 16, 38);
     }
     public create(): void {
         // Basic physsics
@@ -18,17 +19,17 @@ export default class Game extends Phaser.State {
         this.physics.p2.world.setGlobalStiffness(1e5);
 
         // new player
-        this.player = this.add.sprite(200, 200, 'player_walk');
-        this.player.scale.setTo(2);
+        this.player = this.add.sprite(200, 200, 'player_anim');
+        this.player.scale.setTo(3);
         this.physics.p2.enable([this.player]);
         this.player.body.setZeroDamping();
         this.player.body.fixedRotation = true;
 
         // Player animations
-        this.player.animations.add('walk_right', [0, 1, 2, 3, 4, 5, 6, 7], 8, true);
-        this.player.animations.add('walk_left', [8, 9, 10, 11, 12, 13, 14, 15], 8, true);
-        this.player.animations.add('iddle_left', [16, 17, 18], 2, true);
-        this.player.animations.add('iddle_right', [19, 20, 21], 2, true);
+        this.player.animations.add('iddle_l', [0, 1, 2, 3], 4, true);
+        this.player.animations.add('iddle_r', [4, 5, 6, 7], 4, true);
+        this.player.animations.add('walk_r', [8, 9, 10, 11, 12, 13, 14, 15], 8, true);
+        this.player.animations.add('walk_l', [16, 17, 18, 19, 20, 21, 22, 23], 8, true);
 
         // set collisions
         let spriteMaterial = this.physics.p2.createMaterial('spriteMaterial', this.player.body);
@@ -47,7 +48,7 @@ export default class Game extends Phaser.State {
             this.player.body.moveLeft(150);
 
             if (this.facing !== 'left') {
-                this.player.animations.play('walk_left', 15);
+                this.player.animations.play('walk_l', 15);
                 this.facing = 'left';
             }
         }
@@ -55,7 +56,7 @@ export default class Game extends Phaser.State {
             this.player.body.moveRight(150);
 
             if (this.facing !== 'right') {
-                this.player.animations.play('walk_right', 15);
+                this.player.animations.play('walk_r', 15);
                 this.facing = 'right';
             }
         }
@@ -66,20 +67,24 @@ export default class Game extends Phaser.State {
                 //this.player.animations.stop();
 
                 if (this.facing === 'left') {
-                    this.player.animations.play('iddle_right', 6);
+                    this.player.animations.play('iddle_r', 6);
                     //this.player.frame = 17;
                 }
                 else {
-                    this.player.animations.play('iddle_left', 6);
+                    this.player.animations.play('iddle_l', 6);
                     //this.player.frame = 16;
                 }
 
                 this.facing = 'idle';
             }
         }
-        if (this.cursors.up.isDown && this.game.time.now > this.jumpTimer && this.checkIfCanJump())
-        {
+        if (this.cursors.up.isDown && this.game.time.now > this.jumpTimer && this.checkIfCanJump()) {
             this.player.body.moveUp(300);
+            if(this.facing === 'left'){
+
+            }else if(this.facing === 'left'){
+
+            }
             this.jumpTimer = this.game.time.now + 600;
         }
     }
@@ -88,7 +93,7 @@ export default class Game extends Phaser.State {
 
     }
 
-    public checkIfCanJump():boolean{
+    public checkIfCanJump(): boolean {
         return true;
     }
 
